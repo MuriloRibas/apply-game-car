84cdb1677fcb74b9ef38c2b7b198c1370bbcdf8f
MuriloRibas <muriloribasss@hotmail.com>
2020-04-23 17:12:19 -0300

    Added move logic to keys A, S and D.

--- src/components/environment/index.js
+++ src/components/environment/index.js
@@ -1,35 +1,59 @@
 import React, { useState, useEffect } from 'react'
 import environment from '../../assets/gifs/CENARIO_anima.gif'
-import { DivAlignEnv, ImgCar, ImgEnv } from './styles';
+import { DivAlignEnv, ImgEnv } from './styles';
 import CarComponent from '../car/index';
 
 export default function EnvironmentComponent() {
 
     const [carPosition, setCarPosition] = useState(1) // POSIÇÕES (ESQUERDA, MEIO, DIREITA): 1 - 2 - 3 |||| 2 - 40 - 70
 
-    const handleKeyPress = code => {
-        switch(code) {
+    const checkKeyAndPosition = (key, notPosition) => {
+        console.log('KEY: ', key)
+        if (carPosition === notPosition) {
+            return false;
+        }
+        return true
+    }
+
+    const handleKeyPress = e => {
+        switch(e.keyCode) {
             case 37: 
-                console.log('ARROW LEFT');
-                if (carPosition === 1) {
-                    break
+                if ( checkKeyAndPosition(e.key, 1) === false ) {
+                    break;
                 }
                 setCarPosition(carPosition - 1);
                 break;
             case 39: 
-                console.log('ARROW RIGHT');
-                if (carPosition === 3) {
-                    break
+                if ( checkKeyAndPosition(e.key, 3) === false ) {
+                    break;
                 }
                 setCarPosition(carPosition + 1);
                 break;
+            case 65: 
+                if ( checkKeyAndPosition(e.key, 1) === false ) {
+                    break;
+                }
+                setCarPosition(1)
+                break;
+            case 83: 
+                if ( checkKeyAndPosition(e.key, 2) === false ) {
+                    break;
+                }
+                setCarPosition(2)
+                break;
+            case 68:
+                if ( checkKeyAndPosition(e.key, 3) === false ) {
+                    break;
+                }
+                setCarPosition(3)
+                break;
             default:
-                return 1;
+                return 2;
         }
     }
 
     useEffect(() => {
-        window.addEventListener("keydown", (key) => handleKeyPress(key.keyCode));
+        window.addEventListener("keydown", (e) => handleKeyPress(e));
     })
     
     return (
